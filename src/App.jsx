5f0071bbee791efb914d0495d4d37091598c3639
Tahir Dibirov II <dibirovtahir@gmail.com>
2022-12-21 11:09:51 +0400

    INIT

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,39 @@ import './App.css'
 
 function App() {
   const [data, setData] = useState([])
-  const [visibility, setVisibility] = useState(false)
+  const [visibility, setVisibility] = useState(true)
   const [filteredData, setFilteredData] = useState([])
+  const [number, setNumber] = useState(5)
 
   useEffect(() => {
     fetch('https://northwind.now.sh/api/categories')
       .then(response => response.json())
       .then(data => setData(data))
-  }, [])
+      .then(viewData)
+  }, [number])
 
 
   const handler = (event) => {
+    setNumber(event.target.value)
+  }
+
+  const viewData = ()=>{
     setFilteredData([...data].filter((_, index) => {
-      return index < event.target.value
+      return index < number
     }))
   }
 
-
   return (
     <div className="App">
+      <h1>Fetch Data</h1>
      <div className="controller">
-     <input onChange={handler} type="number" name="" id="" />
-      <button onClick={() => { setVisibility(!visibility) }}>GET</button>
+     <input onChange={handler} type="number" name="" id="" min={0} max={7}/>
+      <button onClick={() => { setVisibility(true) 
+      viewData()
+      }}>GET</button>
      </div>
       <ul className='data'>
-          {visibility && filteredData.map(item => {
+          {filteredData.map(item => {
             return(
               <li key={item.id}>{item.name}</li>
             )
